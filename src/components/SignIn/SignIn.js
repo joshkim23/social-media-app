b8f53fd41bfc4cb29cbadf39774f42b7cc3fdcc9
Joshua Kim <joshkim23@gmail.com>
2021-02-19 01:03:40 -0800

    moved styles to inside of component, renamed classes to styles

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// React core
+import React, {useState, useEffect} from 'react';
 
 // Routing 
 import { Link } from 'react-router-dom';
@@ -20,60 +21,57 @@ import Visibility from '@material-ui/icons/VisibilityOff';
 import { makeStyles } from '@material-ui/core/styles';
 import './SignIn.css';
 
-
-
-const useStyles = makeStyles((theme) => ({
-    main: {
-        display: 'grid',
-        gridTemplateColumns: '1fr',
-        backgroundColor: '#f7f1e3',
-        justifySelf:'center',
-        alignItems: 'center',
-        minHeight: '100vh'
-    },
-    signInContainer: {
-        backgroundColor: '#fff',
-        padding: '20px 30px',
-        borderRadius: '10px',
-        display: 'grid',
-        gridTemplateColumns: '1fr',
-        width: 400,
-        justifySelf:'center',
-    },
-    formWithLabel: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    inputLine: {
-        display: 'grid',
-        gridTemplateColumns: '1fr 10fr',
-        width: '100%',
-        alignItems: "center"
-    },
-    form: {
-        width: '100%', 
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(2, 0, 2),
-    },
-}));
-
 const SignIn = () => {
-    const classes = useStyles();
+    const useStyles = makeStyles((theme) => ({
+        main: {
+            display: 'grid',
+            gridTemplateColumns: '1fr',
+            backgroundColor: '#f7f1e3',
+            justifySelf:'center',
+            alignItems: 'center',
+            minHeight: '100vh'
+        },
+        signInContainer: {
+            backgroundColor: '#fff',
+            padding: '20px 30px',
+            borderRadius: '10px',
+            display: 'grid',
+            gridTemplateColumns: '1fr',
+            width: 400,
+            justifySelf:'center',
+        },
+        formWithLabel: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        inputLine: {
+            display: 'grid',
+            gridTemplateColumns: '1fr 10fr',
+            width: '100%',
+            alignItems: "center"
+        },
+        form: {
+            width: '100%', 
+            marginTop: theme.spacing(1),
+        },
+        submit: {
+            margin: theme.spacing(2, 0, 2),
+        },
+    }));
+    const styles = useStyles();
 
   return (
-    <div className={classes.main}>
-        <div className={classes.signInContainer}>
+    <div className={styles.main}>
+        <div className={styles.signInContainer}>
             <CssBaseline />
-            <div className={classes.formWithLabel}>
+            <div className={styles.formWithLabel}>
                 <Typography component="h1" variant="h5">
                 Sign in
                 </Typography>
-                <form className={classes.form} noValidate>
+                <form className={styles.form} noValidate>
 
-                    <div className={classes.inputLine}>
+                    <div className={styles.inputLine}>
                         <Grid item>
                             <AccountCircle />
                         </Grid>
@@ -89,7 +87,7 @@ const SignIn = () => {
                         </Grid>
                     </div>
 
-                    <div className={classes.inputLine}>
+                    <div className={styles.inputLine}>
                         <Grid item>
                             <Visibility />
                         </Grid>
@@ -112,7 +110,7 @@ const SignIn = () => {
                         fullWidth
                         variant="contained"
                         color="primary"
-                        className={classes.submit}
+                        className={styles.submit}
                     >
                         Sign In
                     </Button>
@@ -124,7 +122,7 @@ const SignIn = () => {
                     variant="outlined"
                     fullWidth
                     color="primary"
-                    className={classes.submit}>
+                    className={styles.submit}>
                     Create an account
                 </Button>
             </div>
