b9d4a00083adf2ea2ed358dade0050d7e2dd9ad9
Sammie Kau <sammiekau@Sammies-MacBook-Air.local>
2021-02-19 22:10:05 -0800

    started homepage, creatd header component

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header/Header.js';
 
 const HomePage = ({username}) => {
     return (
         <div>
+            <Header />
             User authenticated, welcome to the home page {username}!!!
         </div>
     )
