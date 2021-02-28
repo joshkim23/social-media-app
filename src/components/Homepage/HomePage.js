451d7ed8fa4ab851b24e7d4c0959df115892811c
Joshua Kim <joshkim23@gmail.com>
2021-02-27 22:09:10 -0800

    overlay css stuff, general layout for homepage

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -2,13 +2,47 @@ import React from 'react';
 import Header from './Header/Header.js';
 
 const HomePage = ({username, signOut}) => {
+    const styles = {
+        layout: {
+            display: 'grid',
+            gridTemplateColumns: '1fr 2fr 1fr',
+            gridGap: '20px',
+            padding: '20px',
+        },
+        content: {
+            border: '1px solid black',
+            borderRadius: '10px',
+            padding: '6px',
+            backgroundColor: '#fff',
+            height: '500px'
+        },
+        overlay: { // need this to make the div FULL screen!!
+            backgroundColor: '#f7f1e3',
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+        }
+    }
     return (
-        <div>
+        <div style={styles.overlay}>
             <Header 
                 username={username}
                 signOut = {signOut}
             />
-            User authenticated, welcome to the home page {username}!!!
+
+            <div style={styles.layout}>
+                <div style={styles.content}>
+                    Users List goes here
+                </div>
+
+                <div style={styles.content}>
+                    create new post and existing posts go here
+                </div>
+
+                <div style={styles.content}>
+                    chat box goes here
+                </div>
+            </div>
         </div>
     )
 }
